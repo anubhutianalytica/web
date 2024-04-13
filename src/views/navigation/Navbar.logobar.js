2916142b854d51b2e7579024eba45d2a352a1e1c
Yash Shrivastava <the.revived.1@gmail.com>
2024-04-13 13:32:11 +0530

    Remove redundant code

--- src/views/navigation/Navbar.logobar.js
+++ src/views/navigation/Navbar.logobar.js
@@ -7,44 +7,28 @@ import "../../styles/Navbar.css";
 export default function NavbarLogobar() {
   return (
     <>
-    <Box sx={{ flexGrow: 1, justifyContent: "flex-start" }}>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#app-bar-with-responsive-menu"
-                sx={{
-                  ml: 2,
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".2rem",
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
-              >
-                <img src={icon} className="App-icon" alt="icon" />
-              </Typography>
-            </Box>
-            <Box sx={{ flexGrow: 40, justifyContent: "flex-start" }}>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#app-bar-with-responsive-menu"
-                sx={{
-                  ml: 2,
-                  display: { xs: "none", md: "flex" },
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".2rem",
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
-              >
-                Anubhuti Analytics
-              </Typography>
-            </Box>
-            </>
+      <Box sx={{ flexGrow: 1, justifyContent: "flex-start" }}>
+          <img src={icon} className="App-icon" alt="icon" />
+      </Box>
+      <Box sx={{ flexGrow: 40, justifyContent: "flex-start" }}>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="#app-bar-with-responsive-menu"
+          sx={{
+            ml: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".2rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Anubhuti Analytics
+        </Typography>
+      </Box>
+    </>
   );
 }
