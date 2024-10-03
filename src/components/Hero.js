718c279bf29274ff21e42141215d07e5e320cc70
Yash Shrivastava <the.revived.1@gmail.com>
2024-10-03 14:39:23 +0530

    Updated call to Action

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,72 +1,105 @@
+import { Button } from "@mui/material";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Hero() {
+  const navigate = useNavigate();
+
   return (
-      <Container
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        pt: { xs: 4, sm: 2 },
+        pb: { xs: 8, sm: 2 },
+      }}
+      className="container"
+    >
+      <Stack
+        spacing={2}
+        useFlexGap
         sx={{
-          display: "flex",
-          flexDirection: "column",
+          width: { xs: "100%", sm: "100%" },
+          flexDirection: { xs: "column", sm: "row" },
+          justifyContent: { xs: "flex-start", sm: "space-between" },
           alignItems: "flex-start",
-          pt: { xs: 4, sm: 2 },
-          pb: { xs: 8, sm: 2 },
         }}
-        className="container"
+        className="about-us-stack"
       >
-        <Stack
-          spacing={2}
-          useFlexGap
+        <Typography
+          variant="h1"
           sx={{
-            width: { xs: "100%", sm: "100%" },
-            flexDirection: { xs: "column", sm: "row" },
-            justifyContent: { xs: "flex-start", sm: "space-between" },
-            alignItems: "flex-start",
+            display: "flex",
+            flexDirection: { xs: "column" },
+            alignSelf: "center",
+            background: {
+              xs: "",
+              sm: "radial-gradient(circle closest-side, rgba(72, 172, 240, 0.35) 0%, rgba(181, 37, 37, 0) 100%)",
+            },
+            textAlign: "center",
+            fontSize: "clamp(3.5rem, 10vw, 4rem)",
           }}
-          className="about-us-stack"
         >
+          Manage Your
           <Typography
+            component="span"
             variant="h1"
             sx={{
-              display: "flex",
-              flexDirection: { xs: "column" },
-              alignSelf: "center",
-              background: {
-                xs: "", sm: "radial-gradient(circle closest-side, rgba(72, 172, 240, 0.35) 0%, rgba(181, 37, 37, 0) 100%)"},
-              textAlign: "center",
-              fontSize: "clamp(3.5rem, 10vw, 4rem)",
+              fontSize: "clamp(3rem, 10vw, 4rem)",
+              color: (theme) =>
+                theme.palette.mode === "light"
+                  ? "primary.main"
+                  : "primary.light",
             }}
           >
-            Delivering
-            <Typography
-              component="span"
-              variant="h1"
-              sx={{
-                fontSize: "clamp(3rem, 10vw, 4rem)",
-                color: (theme) =>
-                  theme.palette.mode === "light"
-                    ? "primary.main"
-                    : "primary.light",
-              }}
-            >
-              Impactful Services
-            </Typography>
+            Growing
           </Typography>
+          Business
+        </Typography>
+        <Stack
+          spacing={2}
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            width: "100%",
+            textAlign: "center",
+            pt: { xs: 4, sm: 10 },
+            pb: { xs: 2, sm: 2 },
+          }}
+        >
           <Typography
-            textAlign={{ xs: "center", sm: "end" }}
-            color="text.secondary"
-            variant="h6"
+            variant="h5"
             sx={{
-              alignSelf: "center",
-              width: { sm: "100%", md: "50%" },
-              pt: 0,
+              lineHeight: "1.4",
+              color: "#333",
+              maxWidth: "80%",
             }}
           >
-            Small & Medium Businesses require customized and affordable analytics and consulting - driven by this principle,
-            we solve business problems of business dashboards, forecasting, marketing analytics, and analytics software.
+            Unlock the full potential of your business with tailored analytics
+            and expert insights.
           </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{
+              padding: "12px 24px",
+              fontSize: "1.1rem",
+              borderRadius: "8px",
+              "&:hover": {
+                backgroundColor: "#155a9a",
+              },
+            }}
+            onClick={() => navigate("/quiz")}
+          >
+            Click here to take the quiz
+          </Button>
         </Stack>
-      </Container>
+      </Stack>
+    </Container>
   );
 }
