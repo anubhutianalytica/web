a8d9ef3764bc162aa042816b235a92bb4437e24f
Yash Shrivastava <the.revived.1@gmail.com>
2024-10-04 00:09:46 +0530

    Redirect after sending mail

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -11,14 +11,17 @@ import {
   Button,
 } from "@mui/material";
 import emailjs from "emailjs-com";
+import { useNavigate } from "react-router-dom";
 
 const Recommendations = ({ recommendations }) => {
+  const navigate = useNavigate(); // Initialize useNavigate
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
     message: "",
   });
+  const [thankYouMessageVisible, setThankYouMessageVisible] = useState(false); // State for thank you message
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -39,6 +42,10 @@ const Recommendations = ({ recommendations }) => {
       .send("service_uz2igzf", "template_9ybq53v", templateParams, "veRG3IhECuVx5HtGC")
       .then((result) => {
         console.log(result.text);
+        setThankYouMessageVisible(true); // Show thank you message
+        setTimeout(() => {
+          navigate("/"); // Redirect to home after 3 seconds
+        }, 3000);
       })
       .catch((error) => {
         console.log(error.text);
@@ -58,126 +65,134 @@ const Recommendations = ({ recommendations }) => {
         mt: 2,
       }}
     >
-      <Typography
-        variant="h5"
-        gutterBottom
-        sx={{ fontWeight: "bold", color: "primary.dark", mb: 3 }}
-      >
-        Our Analysis Recommends the Following Services:
-      </Typography>
-
-      <List
-        sx={{
-          backgroundColor: "secondary.contrastText",
-          borderRadius: 3,
-          p: 2,
-          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        {uniqueRecommendations.length > 0 ? (
-          uniqueRecommendations.map((rec, idx) => (
-            <ListItem key={idx}>
-              <ListItemText
-                primary={rec}
-                primaryTypographyProps={{
-                  color: "#007bff",
-                  fontWeight: "medium",
-                  fontSize: "1rem",
-                }}
-              />
-            </ListItem>
-          ))
-        ) : (
-          <ListItem>
-            <ListItemText
-              primary="No recommendations available."
-              primaryTypographyProps={{ color: "#d9534f" }}
-            />
-          </ListItem>
-        )}
-      </List>
-
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
-        style={{ marginTop: "20px" }}
-      >
-        <Typography
-          variant="body1"
-          sx={{
-            mb: 2,
-            fontWeight: "bold",
-            color: "grey.0",
-            textAlign: "center",
-            fontSize: "1.2rem",
-            bgcolor: "secondary.main",
-            borderRadius: 2,
-            p: 2,
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
-        >
-          🌟 Connect with us and unlock exclusive offers! Get a{" "}
-          <u>20% discount</u> on our services and experience the best we have to
-          offer! 🎉
+      {thankYouMessageVisible ? (
+        <Typography variant="h6" sx={{ color: "green", textAlign: "center" }}>
+          Thank you! Your message has been sent.
         </Typography>
+      ) : (
+        <>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ fontWeight: "bold", color: "primary.dark", mb: 3 }}
+          >
+            Our Analysis Recommends the Following Services:
+          </Typography>
 
-        <FormControl fullWidth variant="outlined" margin="normal">
-          <InputLabel htmlFor="name">Name</InputLabel>
-          <OutlinedInput
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            label="Name"
-            sx={{ bgcolor: "white" }}
-          />
-        </FormControl>
-        <FormControl fullWidth variant="outlined" margin="normal">
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <OutlinedInput
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            label="Email"
-            sx={{ bgcolor: "white" }}
-          />
-        </FormControl>
-        <FormControl fullWidth variant="outlined" margin="normal">
-          <InputLabel htmlFor="phone">Phone</InputLabel>
-          <OutlinedInput
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            label="Phone"
-            sx={{ bgcolor: "white" }}
-          />
-        </FormControl>
-        <FormControl fullWidth variant="outlined" margin="normal">
-          <InputLabel htmlFor="message">Message</InputLabel>
-          <OutlinedInput
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            label="Message"
-            multiline
-            rows={4}
-            sx={{ bgcolor: "white" }}
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          type="submit"
-          color="primary"
-          sx={{ mt: 2 }}
-        >
-          Connect
-        </Button>
-      </Box>
+          <List
+            sx={{
+              backgroundColor: "secondary.contrastText",
+              borderRadius: 3,
+              p: 2,
+              boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            {uniqueRecommendations.length > 0 ? (
+              uniqueRecommendations.map((rec, idx) => (
+                <ListItem key={idx}>
+                  <ListItemText
+                    primary={rec}
+                    primaryTypographyProps={{
+                      color: "#007bff",
+                      fontWeight: "medium",
+                      fontSize: "1rem",
+                    }}
+                  />
+                </ListItem>
+              ))
+            ) : (
+              <ListItem>
+                <ListItemText
+                  primary="No recommendations available."
+                  primaryTypographyProps={{ color: "#d9534f" }}
+                />
+              </ListItem>
+            )}
+          </List>
+
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+            style={{ marginTop: "20px" }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                mb: 2,
+                fontWeight: "bold",
+                color: "grey.0",
+                textAlign: "center",
+                fontSize: "1.2rem",
+                bgcolor: "secondary.main",
+                borderRadius: 2,
+                p: 2,
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+              }}
+            >
+              🌟 Connect with us and unlock exclusive offers! Get a{" "}
+              <u>20% discount</u> on our services and experience the best we have to
+              offer! 🎉
+            </Typography>
+
+            <FormControl fullWidth variant="outlined" margin="normal">
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <OutlinedInput
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                label="Name"
+                sx={{ bgcolor: "white" }}
+              />
+            </FormControl>
+            <FormControl fullWidth variant="outlined" margin="normal">
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <OutlinedInput
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                label="Email"
+                sx={{ bgcolor: "white" }}
+              />
+            </FormControl>
+            <FormControl fullWidth variant="outlined" margin="normal">
+              <InputLabel htmlFor="phone">Phone</InputLabel>
+              <OutlinedInput
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                label="Phone"
+                sx={{ bgcolor: "white" }}
+              />
+            </FormControl>
+            <FormControl fullWidth variant="outlined" margin="normal">
+              <InputLabel htmlFor="message">Message</InputLabel>
+              <OutlinedInput
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                label="Message"
+                multiline
+                rows={4}
+                sx={{ bgcolor: "white" }}
+              />
+            </FormControl>
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              sx={{ mt: 2 }}
+            >
+              Connect
+            </Button>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
