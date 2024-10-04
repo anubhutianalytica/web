6c6e23bb414138d3e3b1e6f30de0d8a2c0b794ba
Yash Shrivastava <the.revived.1@gmail.com>
2024-10-04 22:14:22 +0530

    Remove subtitle and alignment fixes

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,7 +2,7 @@ import { Box, Card, CardContent, Typography, Tooltip } from "@mui/material";
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function BlogCard({ blog }) {
+export default function BlogCard({ blog, showSubtitle = false }) {
   const navigate = useNavigate();
 
   const handleCardClick = (fileName) => {
@@ -53,31 +53,38 @@ export default function BlogCard({ blog }) {
             backgroundColor: "rgba(255, 255, 255, 0.4)", // Semi-transparent background for readability
             padding: "16px",
             textAlign: "center",
-            backdropFilter: "blur(10px)", // Adds a subtle blur effect to the background
+            backdropFilter: "blur(5px)", // Adds a subtle blur effect to the background
           }}
         >
           <Tooltip title={blog.title}>
-          <Typography
-            variant="h5"
-            sx={{
-              textAlign: "center", // Ensures the text is centered properly
-              padding: { xs: "0 10px", sm: "0 20px" }, // Adds some padding for smaller screens
-              fontWeight: "bold",
-              lineHeight: "1.2em",
-              height: "3.4em",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-            }}
-          >
-            {blog.title}
-          </Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                textAlign: "center", // Ensures the text is centered properly
+                padding: { xs: "0 10px", sm: "0 20px" }, // Adds some padding for smaller screens
+                fontWeight: "bold",
+                lineHeight: "1.2em",
+                height: "3.4em",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                alignContent: "center",
+              }}
+            >
+              {blog.title}
+            </Typography>
           </Tooltip>
-          <Typography variant="body2" color="inherit"
-          sx ={{
-            pt: 2
-          }}>
-            {blog.subtitle}
-          </Typography>
+
+          {showSubtitle && (
+            <Typography
+              variant="body2"
+              color="inherit"
+              sx={{
+                pt: 2,
+              }}
+            >
+              {blog.subtitle}
+            </Typography>
+          )}
         </CardContent>
       </Card>
     </Box>
